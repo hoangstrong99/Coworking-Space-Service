afc3709a6d86fbfcf44bb02e8b8c6a92542821d9
nvnhan <nvnhan.dev@gmail.com>
2024-09-26 23:12:51 +0700

    Remove superfluous newline in helm_sql.sh
    A superfluous newline was found and removed to streamline the script's formatting. This change helps maintain consistency and avoids unnecessary whitespace in the code.

--- scripts/helm_sql.sh
+++ scripts/helm_sql.sh
@@ -6,7 +6,6 @@ POSTGRESQL=prj3-postgresql
 # shellcheck disable=SC2034
 POSTGRES_PASSWORD=$(kubectl get secret prj3-postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
 kubectl port-forward svc/"$POSTGRESQL" 5432:5432 &
-
 #PGPASSWORD="$POSTGRES_PASSWORD"  psql -U postgres -d postgres -h 127.0.0.1 -a -f ../db/1_create_tables.sql
 #PGPASSWORD="$POSTGRES_PASSWORD"  psql -U postgres -d postgres -h 127.0.0.1 -a -f ../db/2_seed_users.sql
 #PGPASSWORD="$POSTGRES_PASSWORD"  psql -U postgres -d postgres -h 127.0.0.1 -a -f ../db/3_seed_tokens.sql
